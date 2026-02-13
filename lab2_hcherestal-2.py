528bb2c31afc8b393b527f983920860c45b11dd7
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-12 22:13:44 -0500

    Add required docstring header

---
+++ lab2_hcherestal-2.py
@@ -0,0 +1,7 @@
+'''
+Program Name: Resturant Tip Calculator 
+Author: Henoc Cherestal
+Purpose: This program calculates 15% and 20% tip suggestions based on a user's dinner bill total and displays formatted totals.
+Starter Code: None used.
+Date: 02/10/2026
+'''
