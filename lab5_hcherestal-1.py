5ed1054fef9489e09245f2da4d4958f8f76f251e
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-17 22:41:00 -0500

    Header and import random

---
+++ lab5_hcherestal-1.py
@@ -0,0 +1,10 @@
+'''
+Program Name: Dice Rolling Terms 
+Author: Henoc Cherestal 
+Purpose: Trying to simulate two dice roll results and print the right gambling term. 
+Start Code: None 
+Date 02/17/2026
+'''
+
+import random 
+
