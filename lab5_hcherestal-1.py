ac63cb7c7dbf9d7bae76ca4666c9eeb3deb1aba1
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-17 22:49:22 -0500

    First set of if elif else statements

--- lab5_hcherestal-1.py
+++ lab5_hcherestal-1.py
@@ -16,3 +16,15 @@ while True:
 print(f"\nDie 1: {die1}")
 print(f"Die 2: {die2}")
 print(f"Total: {total}") 
+
+if die1 == 1 and die2 ==1:
+    print("Snake Eyes")
+elif (die1 == 1 and die2 == 2) or (die1 == 2 and die2 == 1):
+    print ("Ace Caught a Deuce")
+elif die1 == 2 and die2 == 2:
+        print("Little Joe from Kokomo")
+elif (die1 == 1 and die2 == 4) or (die1 == 4 and die2 == 1):
+        print("Little Phoebe")
+elif (die1 == 2 and die2 == 3) or (die1 == 3 and die2 == 2):
+        print("Little Phoebe")
+ elif die1 == 3 and die2 == 3:
