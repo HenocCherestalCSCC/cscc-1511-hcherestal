b1d85c07e7077ed108632bb6b2b77a9fb352e1c1
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-17 20:03:44 -0500

    added baseprice taxrate and license rate

--- lab2_hcherestal-2.py
+++ lab2_hcherestal-2.py
@@ -5,3 +5,11 @@ Purpose: This program calculates 15% and 20% tip suggestions based on a user's d
 Starter Code: None used.
 Date: 02/10/2026
 '''
+
+def main() -> None: 
+    base_price: float = 18500.00 
+
+    tax_rate: float = 0.065 #6.5% this is the 6.5 tax 
+    license_rate: float = 0.0125 #1.25% personal chosen license 
+
+
