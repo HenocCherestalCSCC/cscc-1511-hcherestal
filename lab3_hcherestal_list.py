f3a3a1bd84efe212cf2aa5bfdba841c82c6ce960
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-18 22:36:05 -0500

    Initial Commit + Title

---
+++ lab3_hcherestal_list.py
@@ -0,0 +1,7 @@
+'''
+Program Name: Lab 3 Part 1 - Trying to Create and Sort a List 
+Author: Henoc Cherestal
+Purpose: Create a camping list of items, print the number of items, then print the list in alphabetical order
+Starter Code: None 
+Date: 2/11/2026
+''' 
