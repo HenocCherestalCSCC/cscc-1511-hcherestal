c43e3cc218fa4bab05cbe76ef486e14201068f11
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-18 22:50:53 -0500

    Initial Commit + Title and desc

---
+++ lab3_hcheresal_add.py
@@ -0,0 +1,6 @@
+'''Program Name: Lab 3 Part 2 - Adding to my Camping List
+Author: Henoc Cherestal 
+Purpose: I import my list from Part 1 (no c/p, then append 5 more items, then print the end list result reversed alphabetically)
+Starter Code: None 
+Date: 2/11/2026
+'''
