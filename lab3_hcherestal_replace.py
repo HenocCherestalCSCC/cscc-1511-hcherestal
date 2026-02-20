5cf4df8c09384b1b84a55cfebee234712b5e1e3f
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-19 20:47:46 -0500

    spelling

--- lab3_hcherestal_replace.py
+++ lab3_hcherestal_replace.py
@@ -1,7 +1,7 @@
 ''' 
 Program Name: Lab 3 Part 3 - Replace Item in my List 
 Author: Henoc Cherestal 
-Purpsoe: Supposed to import the list from Lab Part 2, then replace one middle item with 'binoculars,' 
+Purpose: Supposed to import the list from Lab Part 2, then replace one middle item with 'binoculars,' 
 then use a notation slice to print the items before binoculars, binoculars, and the items after binoculars.
 Starter Code: None 
 Date 02/16/2026
