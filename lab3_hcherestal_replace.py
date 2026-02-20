c7875fae5ca5ce207023001af8fa28186a79ab58
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-19 20:32:08 -0500

    For got bino item line, started print statements and flow veri

--- lab3_hcherestal_replace.py
+++ lab3_hcherestal_replace.py
@@ -17,8 +17,15 @@ camping_items[replacemiddile_index] = "binoculars"
 def main() -> None: 
     '''printing slices around 'binoculars''''
     binocular_index: int = camping_items.index("binoculars")
+    
+    
     '''binoculars is a bit long to type, now changing to bino'''
     before_bino: List[str] = camping_items[:binocular_index] 
+    bino_item: str = camping_items[:binocular_index]
     '''basically copy above but add list str and add 1 to index''' 
     after_bino: List[str] = camping_items[:binocular_index + 1 :]
-    
+
+    print("Before binoculars:")
+    print(before_bino)
+
+    print ("\nBinoculars item")
