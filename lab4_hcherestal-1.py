a49d7c3849633d4850347fb6185e94b25810151e
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-19 21:46:21 -0500

    Initial add + import random

---
+++ lab4_hcherestal-1.py
@@ -0,0 +1,10 @@
+'''
+Progam Name: Card Dealing 
+Author: Henoc Cherestal 
+Purpose: Trying to ask the user how many cards to deal, then deal that many 
+non-repeating cards and show the hand. 
+Starter Code: None 
+Date: 02/17/2026
+'''
+
+import random
