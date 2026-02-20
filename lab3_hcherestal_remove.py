ffca727cc8655dd6605ef5fe7f0775c0989d5e34
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-19 20:54:00 -0500

    initial add and import of part3

---
+++ lab3_hcherestal_remove.py
@@ -0,0 +1,11 @@
+'''
+Program Name: Lab 3 Part 4 - Removing an item from my list 
+Author: Henoc Cherestal 
+Purpose: I should import the list for part 3 and remove 'binoculars' from the list, 
+then print the completed list. Then I should print hoe many items I am bringing using len().
+Starter Code: None 
+Date: 02/17/2026
+'''
+from typing import List 
+from lab3_hcherestal_replace import camping_items as items_from_part3
+
