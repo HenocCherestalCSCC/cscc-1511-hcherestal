2d93105360ceb2d1fa2d58d979b6a8256fa7716e
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-19 21:49:49 -0500

    definiing the funcion for 'opening' the deck pack

--- lab4_hcherestal-1.py
+++ lab4_hcherestal-1.py
@@ -8,3 +8,8 @@ Date: 02/17/2026
 '''
 
 import random
+
+def open_deck(values: list[str], suits: list[str]) -> list[str]: 
+    #have to explain func
+    '''Deploy a full deck as strings like 10h ot Jc'''
+    
