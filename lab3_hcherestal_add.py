41b6c886d0f35c2ba2304c90f4048305d2945819
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-19 20:43:05 -0500

    renamed file

---
+++ lab3_hcherestal_add.py
@@ -0,0 +1,25 @@
+'''Program Name: Lab 3 Part 2 - Adding to my Camping List
+Author: Henoc Cherestal 
+Purpose: I import my list from Part 1 (no c/p, then append 5 more items, then print the end list result reversed alphabetically)
+Starter Code: None 
+Date: 2/11/2026
+'''
+from typing import List 
+from lab3_hcherestal_list import camping_items as base_items 
+
+camping_items: List[str] = base_items.copy()
+
+camping_items.append("pillow")
+camping_items.append("sunscreen")
+camping_items.append("bug spray")
+camping_items.append("camp chair")
+camping_items.append("knife")  
+
+def main() -> None:
+    print("Alphabetical list reversed (Z-A):")
+    print(sorted(camping_items, reverse=True))
+
+if __name__ == "__main__": 
+    main() 
+
+    #renamed file, code refactored
