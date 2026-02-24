377601ebdfb9e9d7a164a84cecb992a462f98072
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-24 17:54:48 -0500

    Initial Commit, docstring intro and dict import

---
+++ lab6_hcherestal-1.py
@@ -0,0 +1,8 @@
+'''
+Program Name: User Login System 
+Author: Henoc Cherestal 
+Purpose: Simulate a user login system using a dictionary of usernames and passwords, 
+validate the username, and display security level on login. (BONUS CHALLENGE: Limit to 3 incorrect attempts) 
+'''
+
+from typing import Dict, Optional 
