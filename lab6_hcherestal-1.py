1ae2c569bc1a81c5c413aac493521fa32cae2026
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-02-24 22:36:49 -0500

    git issue

--- lab6_hcherestal-1.py
+++ lab6_hcherestal-1.py
@@ -13,7 +13,7 @@ def build_user_db() -> Dict[str, str]:
         "guest": "guest",
         "andywood": "pythonpro!!",
         "hcherestal": "$hootforan_@!",
-        "admin": "Admin#2026",
+        "challenge": "@ccepted!",
     }
 
 def what_security_level(username: str) -> str: 
@@ -21,4 +21,23 @@ def what_security_level(username: str) -> str:
     if username == "guest": 
         return "Guest Access"
     return "Security Level 1" 
+
+def check_password(users: Dict[str, str], username: str, max_attempts: int = 3) -> bool: 
+    """This has to to ask for the user's password up to 3 or max_attempts times. 
+    Return True if password is right, otherwise false. 
+    """
+
+    correct_password: str = users[username]
+
+    for attempt in range (1, max_attempts + 1):
+        password: str = input("Please enter your password: ").strip()
+
+        if password == correct_password:
+            return True
+    
+        if attempt < max_attempts: 
+            print("Access Denied")
+            print(f"Try again ({max_attempts - attempt} attempts(s) left).")
+
+    return False 
 
