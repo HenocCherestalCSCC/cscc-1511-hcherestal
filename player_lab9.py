1307a2df14c5d6483c1eeaf86c96e67b92001211
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-03-24 22:55:41 -0400

    Added Player class with wallet and Coin object

--- player_lab9.py
+++ player_lab9.py
@@ -15,4 +15,28 @@ class Player:
         '''initialize the name wallet and coin'''
         self.__name: str = name
         self.__wallet: int = 20
-        self.__coin: Coin = Coin()
+        self.__coin: Coin = Coin() 
+
+    def toss_coin(self) -> None:
+        '''toss the coin'''
+        self.__coin.toss()
+
+    def get_coin_side(self) -> str:
+        '''which side of coin?'''
+        return self.__coin.get_sideup()
+
+    def win_coin(self) -> None:
+        '''adding coin to wallet'''
+        self.__wallet += 1
+
+    def lose_coin(self) -> None:
+        '''-1 coin from wallet'''
+        self.__wallet -= 1
+
+    def get_wallet(self) -> int:
+        '''return how many coins are left in wallet'''
+        return self.__wallet
+
+    def get_name(self) -> str:
+        '''return the players name'''
+        return self.__name    
