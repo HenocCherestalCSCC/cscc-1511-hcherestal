57f3c5bd1c6d8880bac58775d31147508e68befc
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-03-24 22:39:43 -0400

    initial commit + importing coin class and adding a a player

---
+++ player_lab9.py
@@ -0,0 +1,12 @@
+'''
+Program Name: Player Class
+Author: Henoc Cherestal
+Purpose: This file creates the Player class for the Match Coins game.
+         A player has a name, a wallet of coins, and a Coin object.
+Starter Code: None
+Date: 03/24/2026
+'''
+
+from coin import Coin 
+
+class Player:
