6c104c430cd7b518c97109c4ef8bb6ffe456db80
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-03-24 22:36:07 -0400

    build + add Coin Class to show a single coin and to toss it

--- coin_lab9.py
+++ coin_lab9.py
@@ -9,3 +9,20 @@ Date: 03/23/2026
 ''' 
 
 import random
+
+class Coin:
+
+    def __init__(self) -> None:
+        '''this is to make it have a starting side'''
+        self.__sideup: str = "Heads"
+
+    def toss(self) -> None:
+        '''random toss up and get heads or tails'''
+        if random.randint(0, 1) == 0:
+            self.__sideup = "Heads"
+        else:
+            self.__sideup = "Tails"
+
+    def get_sideup(self) -> str:
+        '''which side is facing up'''
+        return self.__sideup
