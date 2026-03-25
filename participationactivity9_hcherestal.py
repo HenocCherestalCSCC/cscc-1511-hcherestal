7de0f07b3da2670725ae1c829853a07a16c9b6d4
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-03-24 22:06:35 -0400

    added Device class with basic methods

---
+++ participationactivity9_hcherestal.py
@@ -0,0 +1,13 @@
+'''
+Program Name: Device Tracker Example 
+Author: Henoc Cherestal 
+Purpose: This program creates a class that models an IT device that someone might use everyday, like a laptop or desktop. 
+This class stores data about the device
+and has funcitons that let me view and change that data. 
+''' 
+
+class Device:
+    def __init__(self, name, status):
+        self.name = name 
+        self.status = status 
+        
