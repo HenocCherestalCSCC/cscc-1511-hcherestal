2e13d39ac3fbf1242d75c3d3110bf6cad2f35a63
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-03-24 23:44:58 -0400

    Restore Lab 9 files from earlier commits

--- lab9_hcherestal-1.py
+++ lab9_hcherestal-1.py
@@ -19,26 +19,3 @@ def main() -> None:
     print("--- Coin Match Game ---")
     print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
     print(f"{player2.get_name()} has {player2.get_wallet()} coins.")
-
-    play_again = input ("\nDo you want to toss the coins? (y/n): ") 
-
-    while play_again.lower() == "y": 
-        print("\nTossing...") 
-
-        player1.toss_coin()
-        player2.toss_coin()
-
-        side1 = player1.get_coin_side()
-        side2 = player2.get_coin_side()
-
-        print(f"{player1.get_name()} tossed {side1}")
-        print(f"{player2.get_name()} tossed {side2}") 
-
-        if side1 == side2:
-            player1.win_coin()
-            player2.lose_coin()
-            print(f"...It's a Match! {player1.get_name()} wins a coin.")
-        else:
-            player2.win_coin()
-            player1.lose_coin()
-            print(f"...No Match! {player2.get_name()} wins a coin.")
---
+++ player_lab9.py
@@ -0,0 +1,42 @@
+'''
+Program Name: Player Class
+Author: Henoc Cherestal
+Purpose: This file creates the Player class for the Match Coins game.
+         A player has a name, a wallet of coins, and a Coin object.
+Starter Code: None
+Date: 03/24/2026
+'''
+
+from coin_lab9 import Coin 
+
+class Player:
+
+    def __init__(self, name: str) -> None:
+        '''initialize the name wallet and coin'''
+        self.__name: str = name
+        self.__wallet: int = 20
+        self.__coin: Coin = Coin() 
+
+    def toss_coin(self) -> None:
+        '''toss the coin'''
+        self.__coin.toss()
+
+    def get_coin_side(self) -> str:
+        '''which side of coin?'''
+        return self.__coin.get_sideup()
+
+    def win_coin(self) -> None:
+        '''adding coin to wallet'''
+        self.__wallet += 1
+
+    def lose_coin(self) -> None:
+        '''-1 coin from wallet'''
+        self.__wallet -= 1
+
+    def get_wallet(self) -> int:
+        '''return how many coins are left in wallet'''
+        return self.__wallet
+
+    def get_name(self) -> str:
+        '''return the players name'''
+        return self.__name    
