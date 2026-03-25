04350f2f0aeb683942f76fde5a07c7410ea29a36
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-03-24 22:27:01 -0400

    Initial Commit + import random

---
+++ coin_lab9.py
@@ -0,0 +1,11 @@
+'''
+Program Name: Coin Class
+Author: Henoc Cherestal
+Purpose: This file creates the Coin class for the Match Coins game.
+It represents ONE coin that can be tossed and can report
+whether it landed on Heads or Tails.
+Starter Code: None
+Date: 03/23/2026
+''' 
+
+import random
