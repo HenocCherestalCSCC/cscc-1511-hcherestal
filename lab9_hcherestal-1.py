ab3eb4fca63fca0abee35c831c2939acc3bf268c
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-03-24 23:51:00 -0400

    Add game loop and coin toss display

--- lab9_hcherestal-1.py
+++ lab9_hcherestal-1.py
@@ -19,3 +19,17 @@ def main() -> None:
     print("--- Coin Match Game ---")
     print(f"{player1.get_name()} has {player1.get_wallet()} coins.")
     print(f"{player2.get_name()} has {player2.get_wallet()} coins.")
+
+    play_again = input("\nDo you want to toss the coins? (y/n): ")
+
+    while play_again.lower() == "y":
+        print("\nTossing...")
+
+        player1.toss_coin()
+        player2.toss_coin()
+
+        side1 = player1.get_coin_side()
+        side2 = player2.get_coin_side()
+
+        print(f"{player1.get_name()} tossed {side1}")
+        print(f"{player2.get_name()} tossed {side2}")
