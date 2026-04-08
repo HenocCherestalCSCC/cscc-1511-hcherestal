c1b15ad2b73b657207ab850b3c71304c15382880
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-04-08 16:43:13 -0400

    added the file processing logic to count the word frequencies

--- lab10_hcherestal-1.py
+++ lab10_hcherestal-1.py
@@ -12,9 +12,35 @@ from pathlib import Path
 import string 
 
 class WordAnalyzer:
-    #analyzing the wordcounts in the .txt files. 
+    #'''analyzing the wordcounts in the .txt files. '''
     def __init__(self, filepath: str) -> None: 
         #'''set up dictionary of file path'''
         self.__filepath: Path = Path(filepath)
         self.__frequencies: dict[str, int] = {}
+    
+
+#'''reading the file and cleaning the text and counting each word'''
+def process_file(self) -> bool: 
+    try:
+        if not self.__filepath.exists():
+            raise FileNotFoundError
         
+        translation_table = str.maketrans("", "", string.punctuation)
+
+        with self.__filepath.open("r", encoding="utf-8") as file: 
+            for line in file: 
+                clean_line = line.lower().translate(translation_table)
+                words = clean_line.split()
+
+                for word in words: 
+                    if word in self.___frequencies: 
+                        self.__frequencies[word] += 1 
+                    else:
+                        self.__frequencies[word] = 1 
+
+        return True 
+
+    except FileNotFoundError
+        print(f"\nError: The file '{self.__filepath.name}' was not found.")
+        return False  
+    
