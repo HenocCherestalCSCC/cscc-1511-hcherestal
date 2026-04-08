a9d09059382eedd6c56ae19974bdba12e8c1bba3
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-04-08 16:33:12 -0400

    made wordanalyzer class with initializer

--- lab10_hcherestal-1.py
+++ lab10_hcherestal-1.py
@@ -11,3 +11,10 @@ Date: 04/07/2026
 from pathlib import Path 
 import string 
 
+class WordAnalyzer:
+    #analyzing the wordcounts in the .txt files. 
+    def __init__(self, filepath: str) -> None: 
+        #'''set up dictionary of file path'''
+        self.__filepath: Path = Path(filepath)
+        self.__frequencies: dict[str, int] = {}
+        
