10793fa7b22b11b9091f3bd116f5a7085adc6fc4
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-04-08 16:21:23 -0400

    Add lab header and required imports

---
+++ lab10_hcherestal-1.py
@@ -0,0 +1,13 @@
+'''
+Program Name: Word Count Analyzer
+Author: Henoc Cherestal
+Purpose: This program lets the user choose from 4 text files and then
+         analyzes the selected file by counting how many times each word
+         appears. The results are printed in alphabetical order after then.
+Starter Code: None
+Date: 04/07/2026
+'''
+
+from pathlib import Path 
+import string 
+
