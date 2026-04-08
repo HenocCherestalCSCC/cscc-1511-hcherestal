650bc86d7e3bdff26e6dc891a466db231e25a35a
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-04-08 16:58:00 -0400

    add abc word count and report output, fixed so it is in the class indent

--- lab10_hcherestal-1.py
+++ lab10_hcherestal-1.py
@@ -19,28 +19,35 @@ class WordAnalyzer:
         self.__frequencies: dict[str, int] = {}
     
 
-#'''reading the file and cleaning the text and counting each word'''
-def process_file(self) -> bool: 
-    try:
-        if not self.__filepath.exists():
-            raise FileNotFoundError
-        
-        translation_table = str.maketrans("", "", string.punctuation)
+    #'''reading the file and cleaning the text and counting each word'''
+    def process_file(self) -> bool: 
+        try:
+            if not self.__filepath.exists():
+                raise FileNotFoundError
+            
+            translation_table = str.maketrans("", "", string.punctuation)
 
-        with self.__filepath.open("r", encoding="utf-8") as file: 
-            for line in file: 
-                clean_line = line.lower().translate(translation_table)
-                words = clean_line.split()
+            with self.__filepath.open("r", encoding="utf-8") as file: 
+                for line in file: 
+                    clean_line = line.lower().translate(translation_table)
+                    words = clean_line.split()
 
-                for word in words: 
-                    if word in self.___frequencies: 
-                        self.__frequencies[word] += 1 
-                    else:
-                        self.__frequencies[word] = 1 
+                    for word in words: 
+                        if word in self.___frequencies: 
+                            self.__frequencies[word] += 1 
+                        else:
+                            self.__frequencies[word] = 1 
 
-        return True 
+            return True 
 
-    except FileNotFoundError
-        print(f"\nError: The file '{self.__filepath.name}' was not found.")
-        return False  
-    
+        except FileNotFoundError
+            print(f"\nError: The file '{self.__filepath.name}' was not found.")
+            return False  
+
+    def print_report(self) -> None:
+            """Print the words and their counts in alphabetical order."""
+            sorted_words = sorted(self.__frequencies.keys())
+
+            print()
+            for word in sorted_words:
+                print(f"{word:<20}:: {self.__frequencies[word]}")    
