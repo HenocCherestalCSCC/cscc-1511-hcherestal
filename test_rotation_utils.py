558fc3f4917ff7807bb229565b7767444e1d673a
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-04-14 23:11:11 -0400

    Initial commit install and import pytest, import adjrot from rotutilis

---
+++ test_rotation_utils.py
@@ -0,0 +1,14 @@
+''' 
+Program: test_rotation_utils.py
+Author: Henoc Cherestal
+Date: 04/14/2026
+Purpose: This file tests the adjust_rotation() function from rotation_utils.py
+         using pytest. It checks positive values, negative values, wrapped
+         values, and invalid input.
+Starter Code: Uses the provided rotation_utils.py file from class/lab starter code.
+'''
+
+import pytest
+from rotation_utils import adjust_rotation
+
+
