6c8dee43cbffb00e8384aa136edfeea6ba8d6dfb
Henoc Cherestal <hcherestal@student.cscc.edu>
2026-05-05 22:41:36 -0400

    inital commit and imports

---
+++ lab15_hcherestal-1.py
@@ -0,0 +1,13 @@
+"""
+Program Name: Lab15_hcherestal-1.py
+Author: Henoc Cherestal
+Purpose: This program uses matplotlib and the math library to plot a
+heart-shaped math formula. It creates x and y lists from calculated
+values, labels the graph, and saves the final plot as an image file.
+Starter Code: No starter code was used. I used the Lab 15 instructions
+and Chapter 15 concepts from Python Crash Course, 3rd Edition.
+Date: May 5, 2026
+"""
+
+import math
+import matplotlib.pyplot as plt
